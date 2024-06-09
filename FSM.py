f888f90002907a40f8abf0eca5ae9c1a8c71d975
Phuc <phucquangle0503@gmail.com>
2024-06-10 00:32:53 +0700

    update FSM

--- FSM.py
+++ FSM.py
@@ -12,6 +12,10 @@ class FSM:
                 self.mixer1_state()
             elif self.state == 'mixer2':
                 self.mixer2_state()
+            elif self.state == 'mixer3':
+                self.mixer3_state()
+            elif self.state == 'pumpin':
+                self.pumpin_state()
             elif self.state == 'pumpout':
                 self.pumpout_state()
     
@@ -20,17 +24,27 @@ class FSM:
         self.state = 'mixer1'
     
     def mixer1_state(self):
-        print("State: mixer1")
+        print("State: mixer 1")
         time.sleep(10)
         self.state = 'mixer2'
     
     def mixer2_state(self):
-        print("State: mixer2")
+        print("State: mixer 2")
         time.sleep(10)
+        self.state = 'pumpout'
+
+    def mixer3_state(self):
+        print("State: mixer 3")
+        time.sleep(10)
+        self.state = 'pumpin'
+    
+    def pumpin_state(self):
+        print("State: pump in")
+        time.sleep(20)
         self.state = 'pumpout'
     
     def pumpout_state(self):
-        print("State: pumpout")
+        print("State: pump out")
         time.sleep(20)
         self.state = 'end'
 
