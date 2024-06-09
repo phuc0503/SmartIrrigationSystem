b16919e74a7e28e770a597386c7dfee7793d8ec9
Phuc <phucquangle0503@gmail.com>
2024-06-09 23:57:51 +0700

    add config

---
+++ config/adafruit-cred.py
@@ -0,0 +1,12 @@
+AIO_FEED_IDS = ["iot-btl.area1", 
+                "iot-btl.area2", 
+                "iot-btl.area3",
+                "iot-btl.mixer1",
+                "iot-btl.mixer2",
+                "iot-btl.mixer3",
+                "iot-btl.pumpin",
+                "iot-btl.pumpout"
+                ]
+
+AIO_USERNAME = "hungle2002"
+AIO_KEY = ""
