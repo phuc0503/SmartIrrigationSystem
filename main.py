ff9d325a53d2183dd89f06bc8ed541355ba6e442
Phuc <phucquangle0503@gmail.com>
2024-06-10 00:10:55 +0700

    update main

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
 from MyMQTT import *
+import config.adafruit_cred as aio
 
-AIO_FEED_IDS = ["iot-btl.area1", 
-                "iot-btl.area2", 
-                "iot-btl.area3",
-                "iot-btl.mixer1",
-                "iot-btl.mixer2",
-                "iot-btl.mixer3",
-                "iot-btl.pumpin",
-                "iot-btl.pumpout"
-                ]
-
-AIO_USERNAME = "hungle2002"
-AIO_KEY = ""
-
-mqtt_client = MyMQTTClient(AIO_USERNAME, AIO_KEY, AIO_FEED_IDS)
+mqtt_client = MyMQTTClient(aio.AIO_USERNAME, aio.AIO_KEY, aio.AIO_FEED_IDS)
 mqtt_client.connect()
 
 while True:
