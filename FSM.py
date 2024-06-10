330e5e7e825cf209d1064aa719b88bc9b8870e2f
Phuc <phucquangle0503@gmail.com>
2024-06-10 20:14:30 +0700

    update FSM

--- FSM.py
+++ FSM.py
@@ -43,12 +43,32 @@ class FSM:
     
     def mixer2_state(self):
         print("State: mixer 2")
+        m485.modbus485_send(m485_params.relay2_ON)
+        if m485.modbus485_read_adc() == 255:
+            print("Mixer 2 is on")
+        else:
+            print("Cannot turn on mixer 2")
         time.sleep(10)
+        m485.modbus485_send(m485_params.relay2_OFF)
+        if m485.modbus485_read_adc() == 0:
+            print("Mixer 2 is off")
+        else:
+            print("Cannot turn off mixer 2")
         self.state = 'mixer3'
 
     def mixer3_state(self):
         print("State: mixer 3")
+        m485.modbus485_send(m485_params.relay3_ON)
+        if m485.modbus485_read_adc() == 255:
+            print("Mixer 3 is on")
+        else:
+            print("Cannot turn on mixer 3")
         time.sleep(10)
+        m485.modbus485_send(m485_params.relay3_OFF)
+        if m485.modbus485_read_adc() == 0:
+            print("Mixer 3 is off")
+        else:
+            print("Cannot turn off mixer 3")
         self.state = 'pumpin'
     
     def pumpin_state(self):
